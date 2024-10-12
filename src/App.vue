6dca87e5f0d29f63d860cfd6e4cbc2130781b21c
eddiemuhoro <eddiemuhoro@gmail.com>
2024-10-12 15:05:54 +0300

    Add Vue Crash Course

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+const name = ref("Horn Key");
+const status = ref(true);
+const languages = ref(["Vue", "React", "Nodejs", "Angular"]);
+
+const toggleStatus = () => {
+  if (status.value === true) {
+    status.value = false;
+  } else if (status.value === false) {
+    status.value = true;
+  }
+};
+</script>
+
+<template>
+  <h1>View Job</h1>
+  <p v-if="status">{{ name }}</p>
+  <p v-else>No user</p>
+  <h3>Languages</h3>
+  <ul>
+    <li v-for="language in languages" :key="language">{{ language }}</li>
+  </ul>
+
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<style>
+h1 {
+  color: aqua;
+}
+</style>
---
+++ src/AppLegacy.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Horn Key",
+      status: true,
+      languages: ["Vue", "React", "Nodejs"],
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === true) {
+        this.status = false;
+      } else if (this.status === false) {
+        this.status = true;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>View Job</h1>
+  <p v-if="status">{{ name }}</p>
+  <p v-else>No user</p>
+  <h3>Languages</h3>
+  <ul>
+    <li v-for="language in languages" :key="language">{{ language }}</li>
+  </ul>
+
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<style>
+h1 {
+  color: aqua;
+}
+</style>
