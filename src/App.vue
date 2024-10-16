effc0f4aaed6f5ec73dbe4146ede1791da31f6a6
eddiemuhoro <eddiemuhoro@gmail.com>
2024-10-16 18:12:32 +0300

    Refactor router configuration and add new views
    - Removed unused router code in the App.vue file
    - Updated route pathnames in the router/index.js file
    - Added a subtitle prop with a default value in the HomeView.vue file
    - Added new routes for JobAdd and JobsVue components in the router/index.js file
    - Created the HomeView.vue and JobsVue.vue views

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@ import Topbar from "@/components/Topbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
 import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
 
 const title = "Become a Web Dev";
 </script>
 <template>
   <Topbar />
-  <Hero />
-  <HomeCards />
-  <JobListings :limit="4" />
+  <RouterView />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,6 @@
+import JobAdd from "@/components/JobAdd.vue";
+import HomeView from "@/views/HomeView.vue";
+import JobsVue from "@/views/JobsVue.vue";
 import { createWebHistory } from "vue-router";
 import { createRouter } from "vue-router";
 
@@ -5,9 +8,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/jobs",
+      path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsVue,
+    },
+    {
+      path: "/jobs/add",
+      name: "jobs-add",
+      component: JobAdd,
     },
   ],
 });
