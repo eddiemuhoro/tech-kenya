c4f69d92ba68a08d5f5fa937c83afd1378ed3ec4
eddiemuhoro <eddiemuhoro@gmail.com>
2024-10-16 18:13:03 +0300

    Refactor Topbar component to use vue-router for navigation

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+import { RouterLink } from "vue-router";
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
+</script>
 
 <template>
   <nav class="bg-green-700 border-b border-green-500">
@@ -8,20 +15,49 @@
           class="flex flex-1 items-center justify-between md:items-stretch md:justify-between"
         >
           <!-- Logo -->
-          <img
-            class="h-10 w-10 rounded-full"
-            src="https://images.unsplash.com/photo-1641236709008-1b30ad669194?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Vue Jobs"
-          />
-          <span class="hidden md:block text-white text-2xl font-bold ml-2"
-            >Vue Jobs</span
-          >
+          <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
+            <img
+              class="h-10 w-auto rounded-full"
+              src="https://images.unsplash.com/photo-1641236709008-1b30ad669194?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Vue Jobs"
+            />
+            <span class="hidden md:block text-white text-2xl font-bold ml-2">
+              Vue Jobs
+            </span>
+          </RouterLink>
           <div class="md:ml-auto">
-            <div class="flex space-x-4">
-              <button class="text-white font-bold">Home</button>
-              <button class="text-white font-bold">Jobs</button>
-
-              <button class="text-white font-bold">Add Job</button>
+            <div class="flex space-x-2">
+              <RouterLink
+                to="/"
+                :class="[
+                  isActiveLink('/') ? 'bg-green-900 text-white' : 'text-white',
+                  'hover:bg-green-900 hover:text-white rounded-md px-3 py-2',
+                ]"
+              >
+                Home
+              </RouterLink>
+              <RouterLink
+                to="/jobs"
+                :class="[
+                  isActiveLink('/jobs')
+                    ? 'bg-green-900 text-white'
+                    : 'text-white',
+                  'hover:bg-green-900 hover:text-white rounded-md px-3 py-2',
+                ]"
+              >
+                Jobs
+              </RouterLink>
+              <RouterLink
+                to="/jobs/add"
+                :class="[
+                  isActiveLink('/jobs/add')
+                    ? 'bg-green-900 text-white'
+                    : 'text-white',
+                  'hover:bg-green-900 hover:text-white rounded-md px-3 py-2',
+                ]"
+              >
+                Add Job
+              </RouterLink>
             </div>
           </div>
         </div>
