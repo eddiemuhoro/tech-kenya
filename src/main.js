754ed3f91ad755039537509193637cb973656b69
eddiemuhoro <eddiemuhoro@gmail.com>
2024-10-19 03:03:42 +0300

    Refactor main.js to import additional CSS files for PrimeIcons and Vue Toastification

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "primeicons/primeicons.css";
+import "vue-toastification/dist/index.css"; // Add this line
 
 const app = createApp(App);
 
