525533358547410daad3c2d634d13b2f5719270c
eddiemuhoro <eddiemuhoro@gmail.com>
2024-11-10 23:57:20 +0300

    Update Hero component in HomeView to include title and subtitle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,10 @@ import HomeCards from "@/components/HomeCards.vue";
 import JobListings from "@/components/JobListings.vue";
 </script>
 <template>
-  <Hero />
+  <Hero
+    :title="'Get hired by companies who are looking for your skills'"
+    :subTitle="'Get hired by companies who are looking for your skills'"
+  />
   <HomeCards />
   <JobListings :limit="4" :showButton="true" />
 </template>
